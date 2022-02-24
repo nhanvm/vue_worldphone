bf6aadaf2e6e21c73b081f713e92dc8a8845f2bc
nhanvm <minh.nhat.dev@gmail.com>
2022-02-24 22:50:35 +0700

    update

--- src/components/contact/ContactComplete.vue
+++ src/components/contact/ContactComplete.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="row">
-    <div class="col-md-8 mx-auto">
-      <div>
-        <h2 class="text-center mb-3">THANK YOU!</h2>
-        <h3 class="text-center text-success"><font-awesome-icon :icon="['fas', 'check']" /></h3>
-        <p>Thanks a bunch for filling that out. It means a lot to us, just like you do! We really appreciate you giving us a moment of your time today. Thanks for being you.</p>
+    <div class="col-md-6 mx-auto">
+      <div class="text-center">
+        <h2 class="mb-3">THANK YOU!</h2>
+        <h3 class="text-success"><font-awesome-icon :icon="['fas', 'check']" /></h3>
+        <p>Thanks a bunch for filling that out. It means a lot to us, just like you do! We really appreciate you giving us a moment of your time today.<br /> Thanks for being you.</p>
       </div>
     </div>
   </div>
@@ -20,5 +20,8 @@ export default {
   h2 {
     font-size: 32px;
     font-weight: bold
+  }
+  h3 {
+    font-size: 100px;
   }
 </style>
